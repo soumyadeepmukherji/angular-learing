7c4359eaf2911c3999f652d0704416b0f254ba5b
Soumyadeep <soumyadeepm2001@gmail.com>
2026-09-07 17:11:59 +0530

    Reactive Form

--- src/app/components/forms/register-form/register-form.ts
+++ src/app/components/forms/register-form/register-form.ts
@@ -1,9 +1,29 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [ReactiveFormsModule,JsonPipe],
   selector: 'app-register-form',
   styleUrl: './register-form.css',
   templateUrl: './register-form.html',
 })
-export class RegisterForm {}
+export class RegisterForm {
+  registerForm: FormGroup = new FormGroup({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+    emailId: new FormControl("",[Validators.email,Validators.required]),
+    password: new FormControl(),
+    role: new FormControl(),
+    city: new FormControl(),
+    state: new FormControl(),
+    zipCode: new FormControl(),
+    isAccept: new FormControl(),
+  })
+
+  formValue: any
+
+  onSave(){
+    this.formValue = this.registerForm.value
+  }
+}
